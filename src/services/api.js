e42b690bec381a8dff225b40106200841fede358
AbdurRazzaque98 <107128032+AbdurRazzaque98@users.noreply.github.com>
2023-01-12 18:08:42 +0530

    12/1/2023

--- src/services/api.js
+++ src/services/api.js
@@ -1,18 +1,20 @@
 import axios from 'axios'
 
 const URL = "http://localhost:3002/api";
-const accessToken ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6InNoYXJqZWVsc2siLCJfaWQiOiI2M2JlODIxMTc0NGJmMzIzMWQ0Njg4MWQiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NzM0NTQyMTZ9.yJI8gYgC5Ig0SDyltgO4rqbGjFxwaRoQqV1LnmGG3-o'
+const Token ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyTmFtZSI6InNoYXJqZWVsc2siLCJfaWQiOiI2M2JlODIxMTc0NGJmMzIzMWQ0Njg4MWQiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2NzM0NTQyMTZ9.yJI8gYgC5Ig0SDyltgO4rqbGjFxwaRoQqV1LnmGG3-o'
 const authAxios = axios.create({
     baseURL:URL,
     headers:{ 
-        Authorization:`Bearer ${accessToken}`
+        Authorization: `Bearer ${Token}`
     }
+    
 })
 
 export const loginUser=async(admin)=>{
     try {
+        
         return await authAxios.post(`/user/loginUser`,admin)
-      
+
     } catch (error) {
         console.log('Error While calling Admin Login',error);
         
